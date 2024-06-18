dffb563639e76afb1000c13d34cf9544eeef9b48
Ciraldos <andreaciraldo2@gmail.com>
2024-06-18 14:33:28 +0200

    .

---
+++ Giorno_2/Esercitazione_18_06/Persona.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercitazione_18_06
+{
+    internal class Persona
+    {
+        public string _nome;
+        public string _cognome;
+        public int _eta;
+
+        public Persona(string nome, string cognome, int eta)
+        {
+            _nome = nome;
+            _cognome = cognome;
+            _eta = eta;
+        } 
+
+        public string getNome()
+        {
+            return _nome;
+        }
+
+        public string getCognome() { 
+            return _cognome;
+        }
+
+        public int getEta()
+        {
+            return _eta;
+        }
+
+        public void getDettagli()
+        {
+            Console.WriteLine("Nome: " + _nome + " Cognome: " + _cognome + " Età: " + _eta);
+        }
+
+    }
+}
---
+++ Giorno_2/Esercitazione_18_06/Program.cs
@@ -0,0 +1,15 @@
+﻿using Esercitazione_18_06;
+
+internal class Program
+{
+    public static void Main (string[] args)
+    {
+        Persona persona = new("Andrea", "Ciraldo", 24);
+
+        Console.WriteLine(persona.getNome());
+        Console.WriteLine(persona.getCognome());
+        Console.WriteLine(persona.getEta());
+        persona.getDettagli();
+
+    }
+}
