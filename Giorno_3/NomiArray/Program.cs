5d2c18dbf1d66252d4dec678de09c1b3ada0f5c8
Ciraldos <andreaciraldo2@gmail.com>
2024-06-19 16:51:09 +0200

    .

---
+++ Giorno_3/NomiArray/Program.cs
@@ -0,0 +1,53 @@
+﻿public class Array
+{
+    private string[] arrayNomi;
+    private string[] ArrayNomi {
+        get { 
+            return arrayNomi; 
+        }
+        set {
+            arrayNomi = value;
+        } 
+    }
+
+    private void caricaArray()
+    {
+        Console.WriteLine("Inserisci la dimensione dell'array:");
+        int dim = int.Parse(Console.ReadLine());
+        ArrayNomi = new string[dim];
+        
+        for (int i = 0; i < dim; i++)
+        {
+            Console.WriteLine("Inserisci Nome:");
+            ArrayNomi[i] = Console.ReadLine();
+            Console.WriteLine("Nome Aggiunto\n");
+        }
+    }
+
+    private void cercaNome()
+    {
+        Console.WriteLine("Inserisci il nome da cercare:");
+        string nome = Console.ReadLine();
+        for (int i = 0; i < ArrayNomi.Length; i++)
+        {
+            if (nome == ArrayNomi[i])
+                Console.WriteLine($"Nome trovato in posizione {i+1}: {nome}");
+        }
+        
+        
+    }
+
+    
+
+
+    public static void Main(string[] args)
+    {   
+        Array array = new Array();
+        array.caricaArray();
+        for (int i = 0; i < array.arrayNomi.Length; i++)
+        {
+            Console.WriteLine($"Nome in posizione {i}: {array.arrayNomi[i]}");
+        }
+        array.cercaNome();
+    } 
+}
--- Giorno_3/NomiArray/obj/Debug/net8.0/NomiArray.AssemblyInfo.cs
+++ Giorno_3/NomiArray/obj/Debug/net8.0/NomiArray.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Conto Corrente")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NomiArray")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d67cdbb9c0c1c3a8ba56a8624f723bf06333c01")]
-[assembly: System.Reflection.AssemblyProductAttribute("Conto Corrente")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Conto Corrente")]
+[assembly: System.Reflection.AssemblyProductAttribute("NomiArray")]
+[assembly: System.Reflection.AssemblyTitleAttribute("NomiArray")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generato dalla classe WriteCodeFragment di MSBuild.
